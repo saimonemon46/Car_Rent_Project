7dbe7c187806d0a0c2e555508183bf0f04e865f2
saimonemon46 <saimonemon46@gmail.com>
2026-02-13 23:49:31 +0600

    start

---
+++ index.php
@@ -0,0 +1,30 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<section>
+    <div>
+        <h1>Welcome to Car Rental</h1>
+        <p>Find the perfect car for your next adventure!</p>
+    </div>
+</section>
+
+<section>
+    <div>
+        <h1>Find the Best Car for You</h1>
+        <p>Browse our selection of high-quality vehicles to suit your needs and preferences.</p>
+    </div>
+</section>
+    
+</body>
+</html>
